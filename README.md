5cf13b141fe38b7b2957d780d843e1dbb460ebbd
Connor Finneran <finneran.61@osu.edu>
2019-12-13 14:58:41 -0500

    relatively finished version, may add more functionality later

--- README.md
+++ README.md
@@ -9,9 +9,13 @@ Automatically updates a public playlist weekly with 15 random songs from 15 diff
  * picked from another playlist of songs already populated with songs
  * Utilizes Windows Scheduler
  * Updates only when 15 songs are in the pre-populated playlist
-   * how to deal with less than 15 songs? pick from another playlist or don't update at all?
- * maybe display stats somehow in playlist comment? most listened to by me or something?
+   * how to deal with less than 15 songs? - currently doesn't update at all
+ * maybe display stats somehow in playlist comment in the future? most listened to by me or something?
  
 ## Status
 * [x] create git repository/README
 * [x] remove old weekly songs from playlist
+* [x] remove new songs from source playlist
+* [x] add new songs to weekly playlist
+* [x] add checks
+* [x] show when playlist was updated
--- updateFifteenFromFifteen.py
+++ updateFifteenFromFifteen.py
@@ -1,4 +1,5 @@
 import random
+from datetime import datetime
 from personalSpotifyInfo import *
 
 import spotipy
@@ -12,48 +13,82 @@ spotipy_client_id = client_id
 spotipy_client_secret = client_secret
 spotipy_redirect_url = redirect_url
 PLAYLIST_SIZE = 15
+MIN_SOURCE_PLAYLIST_SIZE = 20
+RANDOM_THRESHOLD = 30
  
 token = util.prompt_for_user_token(username, scope, client_id = spotipy_client_id, client_secret = spotipy_client_secret, redirect_uri = spotipy_redirect_url)
 
-if token:
-    spotify = spotipy.Spotify(auth = token)
-    spotify.trace = False
+def main():
+    if token:
+        spotify = spotipy.Spotify(auth = token)
+        spotify.trace = False
 
-    # remove old tracks from playlist
-    response = spotify.user_playlist(username, playlist_id, fields = 'tracks, next')
-    tracksToRemove = []
-    for i, item in enumerate(response['tracks']['items']):
-        track = {
-            "positions": [i],
-            "uri": item['track']['id']
-        }
-        tracksToRemove.append(track)
-    spotify.user_playlist_remove_specific_occurrences_of_tracks(username, playlist_id, tracksToRemove)
+        # check if there are enough songs in our source playlist
+        response = spotify.user_playlist(username, source_playlist_id, fields = 'tracks ,next')
+        source_length = len(response['tracks']['items'])
+        if source_length < MIN_SOURCE_PLAYLIST_SIZE:
+            print("Not enough songs in source playlist")
+            return
+        
+        # remove old tracks from playlist
+        response = spotify.user_playlist(username, playlist_id, fields = 'tracks, next')
+        tracksToRemove = []
+        for i, item in enumerate(response['tracks']['items']):
+            track = {
+                "positions": [i],
+                "uri": item['track']['id']
+            }
+            tracksToRemove.append(track)
+        spotify.user_playlist_remove_specific_occurrences_of_tracks(username, playlist_id, tracksToRemove)
 
-    # find n songs from source playlist
-    response = spotify.user_playlist(username, source_playlist_id, fields = 'tracks ,next')
-    newTracks = []
-    newArtists = []
-    while len(newArtists) < PLAYLIST_SIZE:
-        ran = random.randrange(len(response['tracks']['items'])-1)
-        item = response['tracks']['items'][ran]
-        track = {
-            "positions": [ran],
-            "uri": item['track']['id']
-        }
-        # retrieve main artist id
-        artist = item['track']['artists'][0]['id']
-        if artist not in newArtists:
-            newArtists.append(artist)
-            newTracks.append(track)
+        # find n songs from source playlist
+        response = spotify.user_playlist(username, source_playlist_id, fields = 'tracks ,next')
+        newTracks = []
+        newArtists = []
+        # if source is big enough, pick songs randomly
+        if source_length > RANDOM_THRESHOLD:
+            while len(newArtists) < PLAYLIST_SIZE:
+                ran = random.randrange(len(response['tracks']['items'])-1)
+                item = response['tracks']['items'][ran]
+                track = {
+                    "positions": [ran],
+                    "uri": item['track']['id']
+                }
+                # retrieve main artist id
+                artist = item['track']['artists'][0]['id']
+                if artist not in newArtists:
+                    newArtists.append(artist)
+                    newTracks.append(track)
+        else:
+            for i, item in enumerate(response['tracks']['items']):
+                track = {
+                    "positions": [i],
+                    "uri": item['track']['id']
+                }
+                # retrieve main artist id
+                artist = item['track']['artists'][0]['id']
+                if artist not in newArtists:
+                    newArtists.append(artist)
+                    newTracks.append(track)
+                if len(newArtists) > 14:
+                    break
 
-    # remove these songs from source playlist
-    spotify.user_playlist_remove_specific_occurrences_of_tracks(username, source_playlist_id, newTracks)
+        # remove these songs from source playlist
+        spotify.user_playlist_remove_specific_occurrences_of_tracks(username, source_playlist_id, newTracks)
 
-    # add new songs from source to weekly playlist
-    newTrackIds = (track["uri"] for track in newTracks)
-    spotify.user_playlist_add_tracks(username, playlist_id, newTrackIds)
+        # add new songs from source to weekly playlist
+        newTrackIds = (track["uri"] for track in newTracks)
+        spotify.user_playlist_add_tracks(username, playlist_id, newTrackIds)
 
+        # add description to the weekly playlist
+        currentTime = datetime.now()
+        timeString = currentTime.strftime("Updated: %B %d, %Y")
+        #currently cannot change description with spotipy so i'll just put the time in the title
+        title = "Weekly Fifteen From Fifteen " + timeString
+        spotify.user_playlist_change_details(username, playlist_id, title)
 
-else:
-    print("Cannot get token for ", username)
+    else:
+        print("Cannot get token for ", username)
+
+if __name__ == '__main__':
+    main()
