243949dc5e537f7be01f987629708c70740f6dac
Connor Finneran <finneran.61@osu.edu>
2019-12-12 23:11:24 -0500

    initial commit, tbh nothing works

---
+++ updateFifteenFromFifteen.py
@@ -0,0 +1,35 @@
+import pprint
+import sys
+from personalSpotifyInfo import *
+
+import spotipy
+import spotipy.util as util
+
+username = user
+playlist_id = playlist_id1
+source_playlist_id = playlist_id2
+scope = 'playlist-modify-public'
+spotipy_client_id = client_id
+spotipy_client_secret = client_secret
+spotipy_redirect_url = redirect_url
+ 
+token = util.prompt_for_user_token(username, scope, client_id = spotipy_client_id, client_secret = spotipy_client_secret, redirect_uri = spotipy_redirect_url)
+
+if token:
+    spotify = spotipy.Spotify(auth = token)
+    spotify.trace = False
+
+    response = spotify.user_playlist(username, playlist_id, fields = 'tracks,next')
+
+    tracksToRemove = []
+    for i, item in enumerate(response['tracks']['items']):
+        track = item['track']['uri']
+        tracksToRemove.append(track)
+
+    print(tracksToRemove)
+    
+    spotify.user_playlist_remove_specific_occurrences_of_tracks(username, playlist_id, tracksToRemove)
+
+
+else:
+    print("Cannot get token for ", username)
