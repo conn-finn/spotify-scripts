e43968cc68b3e8055cfa850467563288aeb96b14
Connor Finneran <finneran.61@osu.edu>
2020-06-03 21:57:04 -0400

    added basic album cover page and associated files

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# spotify-fifteen-from-fifteen
+# Spotify Fifteen From Fifteen:
 
 ## Description:
 Automatically updates a public playlist weekly with 15 random songs from 15 different artists, selected from another playlist (which is private and populated with music already), removing the songs currently in the playlist. Python script Utilizes the [Spotipy](https://spotipy.readthedocs.io/en/latest/) library to interact with Spotify's API. 
@@ -13,10 +13,30 @@ Automatically updates a public playlist weekly with 15 random songs from 15 diff
  * maybe display stats somehow in playlist comment in the future? most listened to by me or something?
  
 ## Status
-* [x] create git repository/README
-* [x] remove old weekly songs from playlist
-* [x] remove new songs from source playlist
-* [x] add new songs to weekly playlist
-* [x] add checks
-* [x] show when playlist was updated
-* [x] choose a few songs from pool playlist that have been there the longest
+- [x] create git repository/README
+- [x] remove old weekly songs from playlist
+- [x] remove new songs from source playlist
+- [x] add new songs to weekly playlist
+- [x] add checks
+- [x] show when playlist was updated
+- [x] choose a few songs from pool playlist that have been there the longest
+
+# Judging Tracks By Their Album Cover:
+
+## Description:
+Just creates a cool little html page based on a playlist of songs which i think have cool album covers. Python script Utilizes the [Spotipy](https://spotipy.readthedocs.io/en/latest/) library to interact with Spotify's API.
+
+## Features:
+An Html Page with the following for multiple albums:
+ * Album art
+ * Band Name
+ * Album Name
+ * Previewable track from album
+
+## Status
+- [x] creates website of cover art
+- [x] give artist and album name
+- [x] give a preview of a song from the album
+- [ ] clean up code a bit/rename repository and files to reflect additions
+- [ ] give additional info, maybe webscraped from some album review site
+- [ ] additional features like album descriptions, voting, etc.
---
+++ judgingTracksByAlbumCover.py
@@ -0,0 +1,105 @@
+import random
+from datetime import datetime
+
+from personalSpotifyInfo import *
+
+import spotipy
+import spotipy.util as util
+
+
+playlist_id = album_art_playlist_id
+
+filename = "judgingTracksByTheirAlbumCover.html"
+stylesheet = "stylesheet.css"
+
+
+class albumInfo:
+
+    def __init__(self, name, artist, coverUrl, previewUrl, previewName):
+        self.name = name
+        self.artist = artist
+        self.coverUrl = coverUrl
+        self.previewUrl = previewUrl
+        self.previewName = previewName
+        # preview based on track in source playlist ie not just a random song from the album
+
+    def getId(self):
+        return self.coverUrl.strip().split("image/")[1]
+
+    def getCoverTag(self):
+        return f'<div class="album-container"><img src="{self.coverUrl}"></div>'
+
+    def getInfoTag(self):
+        return f'<div class="album-info"><h2 class="artist-name">{self.artist}</h2><div class="border"></div><h2 class="album-name">{self.name}</h2></div>'
+
+    def getPreviewTag(self): 
+        return f'<div class="preview-container"><audio controls class="preview"><source src="{self.previewUrl}">klewtrklgnk</audio><h4 class="track-name">Preview Track: {self.previewName}</h4></div>'
+
+
+def validate(username, scope, spotipy_client_id, spotipy_client_secret, spotipy_redirect_url):
+
+    token = util.prompt_for_user_token(username, scope, client_id = spotipy_client_id, client_secret = spotipy_client_secret, redirect_uri = spotipy_redirect_url)
+    if not token:
+        print("Cannot get token for ", username, "\nquitting")
+        quit()
+    return token
+
+#returns a list of albumInfo objects
+def handleResponse(response):
+    tracks = response['tracks']['items']
+    if len(tracks) < 1:
+        print("No songs in playlist...\nquitting")
+        quit()
+
+    albums = []
+    for trackObj in tracks:
+        track = trackObj['track']
+        cover = track['album']['images'][0]['url']
+        artist = track['album']['artists'][0]['name']
+        album = track['album']['name']
+        preview = track['preview_url']
+        previewName = track['name']
+        albums.append(albumInfo(album, artist, cover, preview, previewName))
+
+    if len(albums) < 1:
+        print("Unable to get album art....\nquitting")
+        quit()
+    return albums
+
+def writeHTMLFile(filename, albums):
+    with open(filename, 'r') as f:
+        f.read()
+    f = open(filename, 'w')
+
+    boilerplateHTML = [f'<html><head><link rel="stylesheet" href="{stylesheet}"></head><body>', f'</body></html>']
+    
+    f.write(boilerplateHTML[0])
+    f.write(f'<div id="content">')
+    
+    for album in albums:
+        content = ""
+        content += album.getCoverTag()
+        content += album.getInfoTag()
+        content += album.getPreviewTag()
+        f.write(content)
+
+    f.write("</div>")
+    f.write(boilerplateHTML[1])
+    f.close()
+
+def main():
+    username = user
+    scope = 'playlist-modify-public playlist-modify-private'
+    
+    token = validate(username, scope, client_id, client_secret, redirect_url)
+
+    spotify = spotipy.Spotify(auth = token)
+    spotify.trace = False
+    
+    response = spotify.user_playlist(username, playlist_id, fields = 'tracks ,next')
+
+    albumInfoList = handleResponse(response)
+    writeHTMLFile(filename, albumInfoList)
+    
+
+main()
